d4cff256e9305e61075f0211f945dae005ebb430
jiale.zhu <jiale.zhu@byton.com>
2019-12-19 15:05:01 +0800

    quickSort

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,60 +1,52 @@
 package sort;
 
-/**
- * 快速排序
- *
- * @author CaMnter
- */
+import java.util.Arrays;
+
 public class QuickSort {
-    public <T extends Comparable<T>> void quickSorting(T[] array, int left, int right) {
-        int r, l;
-        if (left > right) return;
-        // 基准数
-        T temp = array[left];
-        l = left;
-        r = right;
-        while (l != r) {
-            /**
-             * 要先从右往左找
-             * 如果比基准数大，就一直找下去，直到找到比基准数小的
-             */
-            while ((array[r].compareTo(temp) >= 0) && l < r) r--;
-            /**
-             * 要先从左往右找
-             * 如果比基准数小，就一直找下去，直到找到比基准数大的
-             */
-            while ((array[l].compareTo(temp) <= 0) && l < r) l++;
-            if (l < r) {
-                /**
-                 * 交换
-                 */
-                T t = array[l];
-                array[l] = array[r];
-                array[r] = t;
-            }
-        }
-        /**
-         * 基准数与中间位置的数字交换
-         */
-        array[left] = array[l];
-        array[l] = temp;
-        /**
-         * 继续处理左边的,这里是一个递归的过程
-         */
-        quickSorting(array, left, l - 1);
-        /**
-         * 继续处理右边的,这里是一个递归的过程
-         */
-        quickSorting(array, l + 1, right);
+    public static void main(String[] args) {
+        int[] arr = {3, 44, 38, 5, 47, 36, 26, 2, 36, 4, 19, 50, 48};
+        System.out.println("arr = " + Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
     }
 
-    public static void main(String args[]) {
-        Integer[] object = {26, 19, 7, 37, 27, 57, 67, 99, 87, 17};
-        QuickSort quickSorting = new QuickSort();
-        quickSorting.quickSorting(object, 0, object.length - 1);
-        System.out.println("\n快速排序\n");
-        for (int i : object) {
-            System.out.print(i + " ");
+    private static void quickSort(int[] arr, int low, int high) {
+        int pivot;
+        if (low < 0 || high >= arr.length || low >= high) {
+            return;
         }
+        pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
     }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivotVal = arr[low];
+
+        // swap lower item to the tempPivotIndex, then the tempPivotIndex move forward.
+        int tempPivotIndex = low + 1;
+        for (int i = tempPivotIndex; i <= high; i++) {
+            if (arr[i] < pivotVal) {
+                swap(arr, tempPivotIndex, i);
+                tempPivotIndex++;
+            }
+        }
+
+        int pivot = tempPivotIndex - 1;
+        swap(arr, low, pivot);
+
+        System.out.println(String.format("low=%s, high=%s, pivot=%s", low, high, pivot));
+        System.out.println("arr = " + Arrays.toString(arr));
+
+        return pivot;
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        if (index1 != index2) {
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+
+    }
+
 }
